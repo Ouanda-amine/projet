605bbe11f849d4d6731e435b7b258d8afc871e09
amiine <amineouanda@gmail.com>
2024-11-22 09:24:19 +0100

    projet

--- taches.c
+++ taches.c
@@ -1,66 +1,60 @@
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
 
-
-#define Max 100
+#define MAX 100
 
 struct tache {
-    int id ;
+    int id;
     char titre[30];
     char description[100];
-    char dateEch[100];
+    char  dateEch[100];
     char priorite[20];
 };
-
-void CreerTache( struct tache *t , int id ){
-
-    t->id=id;
-    printf("entrez le titre du tache : ");
+void CreatTache(struct tache *t, int idd){
+    t->id=idd;
+    printf("entrez le titre du tache :  ");
     scanf("%s",t->titre);
-    printf("entrez la description du tache : ");
+    printf("entrez la description tache :  ");
     scanf("%s",t->description);
-    printf("entrez la date d'échéance du tache : ");
+    printf("entrez la  date d'echeance du tache :  ");
     scanf("%s",t->dateEch);
-    printf("entrez la priorité du tache : ");
+    printf("entrez le priorite du tache :  ");
     scanf("%s",t->priorite);
 
+}
+void afficherTache(struct tache t ){
+  if(t.id != 0 ){
+    printf("l'id _  : %d \n",t.id);
+
+    printf("la tache _  : %s \n",t.titre);
+
+    printf("la description _  : %s \n",t.description);
+
+    printf("la date _  : %s \n",t.dateEch);
+
+    printf("la priorite _  : %s \n ___________________________ \n",t.priorite);
+  }
 
 }
-void afficherTaches(struct tache t){
-    printf("id __ : %d  . \n",t.id);
-
-    printf("Le titre   __ : %s  . \n",t.titre);
-
-    printf("la description __ : %s  . \n",t.description);
-
-    printf("la date d'échéance : %s  . \n",t.dateEch);
-
-    printf("la priorité  __ : %s  . \n",t.priorite);
-
-
-}
-void modifierTache(struct tache *t , int monchoix , char nouvelleV[]){
-    switch(monchoix){
-        case 1 : 
-        strcpy(t->description,nouvelleV);
+void modifierTache(struct tache *t,int determin , char newval[]){
+    switch(determin){
+        case 1 :
+        strcpy(t->description,newval);
         break;
-        case 2 : 
-        strcpy(t->dateEch,nouvelleV);
+        case 2 :
+        strcpy(t->dateEch,newval);
         break;
-        case 3 :
-        strcpy(t->priorite,nouvelleV);
+        case 3 : 
+        strcpy(t->priorite,newval);
         break;
 
         default : 
-        printf("choix indisponible");
+        printf("indisponible");
         break;
-
-
     }
 }
-void SupprimerTache(struct tache *t){
-
+void supprimerTache(struct tache *t){
     t->id=0;
     strcpy(t->titre,"");
     strcpy(t->description,"");
@@ -69,70 +63,90 @@ void SupprimerTache(struct tache *t){
 
     printf("suppression avec succes");
 
-
-
-
-    
-    
 }
+void filtreTache(struct tache *t , char prio[20]){
+    if(strcmp(t->priorite,prio)==0){
+        printf("l'id _  : %d \n",t->id);
 
+        printf("la tache _  : %s \n",t->titre);
+
+        printf("la description _  : %s \n",t->description);
+
+        printf("la date _  : %s \n",t->dateEch);
+
+        printf("la priorite _  : %s \n",t->priorite);
+    }
+}
 int main(){
-
-    struct tache  lestaches[Max]={0};
-    int choix;
+    struct tache lestaches[MAX];
     int id =0;
+    int choix;
+    int idmod,lechoix;
+    char new[30];
+    int idsup ;
+    char laprio[20];
 
-    do{
-        printf("entrez 1 pour la creation d'une tache . \n  entrez 2 pour l'affichage d'une tache . \n  entrez 3 pour la modification d'une tache . \n  entrez 4 pour supprimer une tache \n __________  :  ");
+            
+    
+
+    do {
+        printf(" 1 creer \n 2 afficher \n 3 modifier \n 4 supprimer     ");
         scanf("%d",&choix);
 
-        switch(choix){
-            case 1 : 
-            CreerTache(&lestaches[id],id +1);
+        switch (choix){
+            case 1 :
+             CreatTache(&lestaches[id],id+1);
             id++;
             break;
             case 2 : 
-            for (int i = 0;i<id;i++){
-                afficherTaches(lestaches[i]);
-                
+            for (int i  =0 ; i<id ; i++){
+                    afficherTache(lestaches[i]);
+                    
             }
             break;
-            
             case 3 :
-            char new[100];
-            int idchange;
-            int lechoix;
+           
 
-            printf("entrez l'id du tache");
-            scanf("%d",&idchange);
+            printf("entrez l'id du tache a modifier : ");
+            scanf("%d",&idmod);
 
-            printf("pour modifier la discription entrez 1  . \n pour modifier la  date d'échéance entrez 2  . \n pour modifier la priorite entrez 3  . \n entrez votre choix__ : ");
+            printf("1 modifier description \n 2 modifier dateEcheante \n 3 modifier priorite ");
             scanf("%d",&lechoix);
 
             printf("entrez votre modification : ");
             scanf("%s",new);
 
-            modifierTache(&lestaches[idchange-1],lechoix,new);
-            break;
-            case 4 : 
-            int idsup;
+            modifierTache(&lestaches[idmod-1],lechoix,new);
 
-            printf("entrez l'id du tache a supprimer");
+            break;
+
+            case 4 :
+            printf("entrez l 'id du tache a supprimer : ");
             scanf("%d",&idsup);
 
-            SupprimerTache(&lestaches[idsup-1]);
+            supprimerTache(&lestaches[idsup-1]);
             break;
-            default:
-            printf("choix indisponible");
+            case 5 :
+            printf("entrez la priorité (high ou low) : ");
+            scanf("%s",laprio);
+            
+            
+             for (int i  =0 ; i<id ; i++){
+                    filtreTache(&lestaches[i],laprio);
+                    
+            }
+            break;
+
+
+
+    
+
+           
+            default : 
+            printf("error");
             break;
 
             
         }
-
-
-    }while(choix !=0);
-
-
+    }while(choix != 0);
 }
-
-
